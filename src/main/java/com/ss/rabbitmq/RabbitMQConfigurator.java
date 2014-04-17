540186bbce786038cf498dd42133ea8e9776eaea
supunkamburugamuva <supun06@gmail.com>
2014-04-17 11:23:06 -0400

    adding more

---
+++ src/main/java/com/ss/rabbitmq/RabbitMQConfigurator.java
@@ -0,0 +1,11 @@
+package com.ss.rabbitmq;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.Serializable;
+
+public interface RabbitMQConfigurator extends Serializable {
+    ConnectionFactory getConnectionFactory();
+
+
+}
--- src/main/java/com/ss/rabbitmq/RabbitMQSpout.java
+++ src/main/java/com/ss/rabbitmq/RabbitMQSpout.java
@@ -1,5 +1,91 @@
 package com.ss.rabbitmq;
 
-public class RabbitMQSpout extends BaseRichSpout {
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import com.rabbitmq.client.*;
+import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+public class RabbitMQSpout extends BaseRichSpout {
+    private ConnectionFactory connectionFactory;
+    private int prefetchCount;
+    private String queueName;
+    private boolean requeueOnFail;
+    private Logger logger;
+
+    private Connection connection;
+    private Channel channel;
+    private QueueingConsumer consumer;
+    private String consumerTag;
+
+    public enum State {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+
+    @Override
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+
+    }
+
+    @Override
+    public void nextTuple() {
+
+    }
+
+    protected boolean isAutoAcking() {
+        return true;
+    }
+
+    private void reset() {
+        consumerTag = null;
+    }
+
+    private void reinitIfNecessary() {
+        if (consumerTag == null || consumer == null) {
+            close();
+            open();
+        }
+    }
+
+    public void open() {
+        try {
+            connection = createConnection();
+            channel = connection.createChannel();
+            if (prefetchCount > 0) {
+                logger.info("setting basic.qos / prefetch count to " + prefetchCount + " for " + queueName);
+                channel.basicQos(prefetchCount);
+            }
+
+            consumer = new QueueingConsumer(channel);
+            consumerTag = channel.basicConsume(queueName, isAutoAcking(), consumer);
+        } catch (Exception e) {
+            reset();
+            logger.error("could not open listener on queue " + queueName);
+            reporter.reportError(e);
+        }
+    }
+
+    private Connection createConnection() throws IOException {
+        Connection connection = connectionFactory.newConnection(Executors.newScheduledThreadPool(10));
+        connection.addShutdownListener(new ShutdownListener() {
+            @Override
+            public void shutdownCompleted(ShutdownSignalException cause) {
+                logger.error("shutdown signal received", cause);
+                reporter.reportError(cause);
+                reset();
+            }
+        });
+        logger.info("connected to rabbitmq: " + connection + " for " + queueName);
+        return connection;
+    }
 }
